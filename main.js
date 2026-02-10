c0fb61ca381f6b9013f484225593152b6b94a58d
gabriele-fontana <gabriele.fontana2001@gmail.com>
2026-02-10 04:23:34 +0100

    add function

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 
 /* crea funzione 'isPalindroma */
-    // Rendiamo la parola minuscola
-   
+function isPalindroma(word) {
+    /* Rendiamo la parola minuscola */
+   const lwrcaseWord = word.toLowerCase(); 
+   const rvrsWord = lwrcaseWord.split('').reverse().join('');
 
-    // Invertiamo la parola: split trasforma in array, reverse lo inverte, join ricrea la stringa
+    /* Invertiamo la parola: split trasforma in array, reverse lo inverte, join ricrea la stringa */
+return lwrcaseWord === rvrsWord;
+}
 
-
-// Chiediamo l'inserimento all'utente tramite prompt
+/* Chiediamo l'inserimento all'utente tramite prompt */
