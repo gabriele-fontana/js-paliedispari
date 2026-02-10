dc126187b5180ca8bb1410700808eb86bba1d5ef
gabriele-fontana <gabriele.fontana2001@gmail.com>
2026-02-10 05:11:34 +0100

    establish the winner

--- main.js
+++ main.js
@@ -1,14 +1,38 @@
 
 
+/* crea funzione 'isPalindroma */
+function isPalindroma(word) {
+    /* Rendiamo la parola minuscola */
+   const lwrcaseWord = word.toLowerCase(); 
+   const rvrsWord = lwrcaseWord.split('').reverse().join('');
+
+    /* Invertiamo la parola: split trasforma in array, reverse lo inverte, join ricrea la stringa */
+return lwrcaseWord === rvrsWord;
+}
+
+/* Chiediamo l'inserimento all'utente tramite prompt */
+const userWord = prompt("Inserisci una parola:");
+
+if (userWord) {
+    if (isPalindroma(userWord)) {
+        console.log(`"${userWord}" è palindroma!`);
+        alert(`"${userWord}" è palindroma!`);
+    } else {
+        console.log(`"${userWord}" non è palindroma.`);
+        alert(`"${userWord}" non è palindroma.`);
+    }
+}
 
 
 /* add prompts */
 /* even or odd prompt */
-const userChoice = prompt("Scegli 'pari' o 'dispari':").toLowerCase();
+const userChoice = prompt("Scegli 'even' o 'odd':").toLowerCase();
 /* 1 to 5 number prompt */
 const userNumb = parseInt(prompt("Inserisci un numero da 1 a 5:"));
 /* generate a random number for pc */
 const computerNumb = Math.floor(Math.random() * 5) + 1;
+console.log(computerNumb);
+
 /* add userNumb and computerNumb */
 const sum = userNumb + computerNumb
 /* create function to verify if sum is even or odd */
@@ -17,4 +41,11 @@ function isEven(number){
 }
 const result = isEven(sum)
 /* print a message to declare the winner */
-
+if (userChoice === result){
+    alert('hai vinto')
+    console.log('hai vinto');
+    
+}else{
+    console.log('ha vinto il computer');
+    alert('ha vinto il computer')
+}
