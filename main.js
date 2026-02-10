2d1bc40add5cc2d32ee2dd12bc4b64733a6a8be2
gabriele-fontana <gabriele.fontana2001@gmail.com>
2026-02-10 04:53:53 +0100

    create is even function and sum User number and computernumber

--- main.js
+++ main.js
@@ -1,26 +1,4 @@
 
-/* crea funzione 'isPalindroma */
-function isPalindroma(word) {
-    /* Rendiamo la parola minuscola */
-   const lwrcaseWord = word.toLowerCase(); 
-   const rvrsWord = lwrcaseWord.split('').reverse().join('');
-
-    /* Invertiamo la parola: split trasforma in array, reverse lo inverte, join ricrea la stringa */
-return lwrcaseWord === rvrsWord;
-}
-
-/* Chiediamo l'inserimento all'utente tramite prompt */
-const userWord = prompt("Inserisci una parola:");
-
-if (userWord) {
-    if (isPalindroma(userWord)) {
-        console.log(`"${userWord}" è palindroma!`);
-        alert(`"${userWord}" è palindroma!`);
-    } else {
-        console.log(`"${userWord}" non è palindroma.`);
-        alert(`"${userWord}" non è palindroma.`);
-    }
-}
 
 
 
@@ -32,7 +10,11 @@ const userNumb = parseInt(prompt("Inserisci un numero da 1 a 5:"));
 /* generate a random number for pc */
 const computerNumb = Math.floor(Math.random() * 5) + 1;
 /* add userNumb and computerNumb */
-
+const sum = userNumb + computerNumb
 /* create function to verify if sum is even or odd */
-
+function isEven(number){
+    return number % 2 === 0;
+}
+const result = isEven(sum)
 /* print a message to declare the winner */
+
