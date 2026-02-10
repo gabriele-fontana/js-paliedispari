3f55cadd9d2cc8dedd2d4af55d90cb11c15e1266
gabriele-fontana <gabriele.fontana2001@gmail.com>
2026-02-10 04:18:06 +0100

    human code

---
+++ main.js
@@ -0,0 +1,9 @@
+
+/* crea funzione 'isPalindroma */
+    // Rendiamo la parola minuscola
+   
+
+    // Invertiamo la parola: split trasforma in array, reverse lo inverte, join ricrea la stringa
+
+
+// Chiediamo l'inserimento all'utente tramite prompt
