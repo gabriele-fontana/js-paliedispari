7d88530a3e8364313216a1c83268bfcdd7267314
gabriele-fontana <gabriele.fontana2001@gmail.com>
2026-02-10 09:51:30 +0100

    use for cicle to reverse the word

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
 
 
-/* crea funzione 'isPalindroma */
-function isPalindroma(word) {
-    /* Rendiamo la parola minuscola */
-   const lwrcaseWord = word.toLowerCase(); 
-   const rvrsWord = lwrcaseWord.split('').reverse().join('');
-
-    /* Invertiamo la parola: split trasforma in array, reverse lo inverte, join ricrea la stringa */
-return lwrcaseWord === rvrsWord;
-}
-
-/* Chiediamo l'inserimento all'utente tramite prompt */
-const userWord = prompt("Inserisci una parola:");
-
-if (userWord) {
-    if (isPalindroma(userWord)) {
-        console.log(`"${userWord}" è palindroma!`);
-        alert(`"${userWord}" è palindroma!`);
-    } else {
-        console.log(`"${userWord}" non è palindroma.`);
-        alert(`"${userWord}" non è palindroma.`);
+    /* crea funzione 'isPalindroma */
+    function isPalindroma(word) {
+        /* Rendiamo la parola minuscola */
+    const lwrcaseWord = word.toLowerCase(); 
+    let rvrsWord = "";/* utilizzo il ciclo for per invertire la parola */
+    for (let i = lwrcaseWord.length - 1; i >= 0; i--) {
+        rvrsWord += lwrcaseWord[i];
     }
-}
+
+        
+    return lwrcaseWord === rvrsWord;
+    }
+
+    /* Chiediamo l'inserimento all'utente tramite prompt */
+    const userWord = prompt("Inserisci una parola:");
+
+    if (userWord) {
+        if (isPalindroma(userWord)) {
+            console.log(`"${userWord}" è palindroma!`);
+            alert(`"${userWord}" è palindroma!`);
+        } else {
+            console.log(`"${userWord}" non è palindroma.`);
+            alert(`"${userWord}" non è palindroma.`);
+        }
+    }
 
 
 /* add prompts */
